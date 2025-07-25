a4c532def0a4bb49998f40ad91e26e6ca5175c71
Pratheeth Udupa <145535376+NoiseBridge7@users.noreply.github.com>
2025-07-25 18:26:33 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,5 @@
 import {io} from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://draw-and-guess-server-production.up.railway.app/", {
+  transports: ["websocket"]);
 export default socket;
